e1755a93b4daaa9a100f7bedb46db842ed244376
Lidi <lidi.mayra@gmail.com>
2015-08-06 16:12:04 -0300

    Correções finais para apresentação

--- spec/controllers/blogs/posts_controller_spec.rb
+++ spec/controllers/blogs/posts_controller_spec.rb
@@ -2,228 +2,244 @@ require 'rails_helper'
 
 RSpec.describe Blogs::PostsController, :type => :controller do
 
-  let(:user){ create(:user) }
-  let(:saved_post){ create(:post) }
+  # No Better Specs
 
-  describe '#index' do
-    subject { get :index }
+  describe 'the index method for posts controller' do
 
-    it { expect(subject).to be_success }
+    it 'should return http success on index' do
+      get :index
+      response.should be_success
+    end
 
-    it "assigns @posts" do
+    it "should assign @posts" do
       expect(assigns(:post).to_a).to match_array(Post.all)
     end
 
     context "when rendering views" do
       render_views
 
-      context "without collection data" do
-        it "renders template" do
-          expect(subject).to render_template(:index)
-        end
+      it "should render template without collection data" do
+        get :index
+        expect(response).to render_template(:index)
       end
 
-      context "with collection data" do
+      it "should render template with collection data" do
+        @user = User.create(
+          email: Faker::Internet.email,
+          password: 'pass1234'
+        )
 
-        before { create_list :post, 3 }
-
-        it "renders template" do
-          expect(subject).to render_template(:index)
+        3.times do
+          Post.create(
+            title: Faker::Lorem.sentence(2),
+            content: Faker::Lorem.sentence(3, true),
+            user_id: @user.id
+          )
         end
+        get :index
+        expect(response).to render_template(:index)
       end
     end
   end
 
-  describe '#new' do
+  # Better Specs
 
-    before { sign_in user }
+  # let(:user){ create(:post) }
+  # let(:saved_post){ create(:post) }
 
-    it 'assigns @post' do
-      get :new
+  # describe '#new' do
 
-      expect(assigns(:post)).to be_a_new(Post)
-    end
+  #   before { sign_in user }
 
-    context "when rendering views" do
-      render_views
+  #   it 'assigns @post' do
+  #     get :new
 
-      it "renders template" do
-        get :new
+  #     expect(assigns(:post)).to be_a_new(Post)
+  #   end
 
-        expect(response).to render_template(:new)
-      end
-    end  
-  end
+  #   context "when rendering views" do
+  #     render_views
+
+  #     it "renders template" do
+  #       get :new
+
+  #       expect(response).to render_template(:new)
+  #     end
+  #   end 
+  # end
  
-  describe '#create' do
+  # describe '#create' do
 
-    before { sign_in user }
+  #   before { sign_in user }
 
-    context 'with params' do
-      it do
-        is_expected.to permit(:title, 
-          :content,
-          :user_id).for(:create, params: { post: attributes_for(:post) })
-      end
-    end
+  #   context 'with params' do
+  #     it do
+  #       is_expected.to permit(:title,
+  #         :content,
+  #         :user_id).for(:create, params: { post: attributes_for(:post) })
+  #     end
+  #   end
 
-    context 'with valid attributes' do
+  #   context 'with valid attributes' do
 
-      subject { post :create, post: attributes_for(:post) }
+  #     subject { post :create, post: attributes_for(:post) }
 
-      it 'creates a new post' do
-        expect { subject }.to change(Post, :count).by(1)
-      end
+  #     it 'creates a new post' do
+  #       expect { subject }.to change(Post, :count).by(1)
+  #     end
 
-      it { is_expected.to redirect_to blogs_root_path }
-    end
+  #     it { is_expected.to redirect_to blogs_root_path }
+  #   end
 
-    context 'with invalid attributes' do
+  #   context 'with invalid attributes' do
 
-      subject { post :create, post: attributes_for(:post).except(:title) }
+  #     subject { post :create, post: attributes_for(:post).except(:title) }
 
-      it 'doesn\'t create post' do
-        expect { subject }.to_not change(Post, :count)
-      end
+  #     it 'doesn\'t create post' do
+  #       expect { subject }.to_not change(Post, :count)
+  #     end
 
-      it { is_expected.to render_template :new }
-    end
-  end
+  #     it { is_expected.to render_template :new }
+  #   end
+  # end
 
-  describe '#show' do
+  # describe '#show' do
 
-    before { get :show, id: saved_post }
+  #   before { get :show, id: saved_post }
 
-    it 'assigns @post' do
-      expect(assigns(:post)).to eq(saved_post)
-    end
+  #   it 'assigns @post' do
+  #     expect(assigns(:post)).to eq(saved_post)
+  #   end
 
-    context 'when rendering views' do
-      render_views
+  #   context 'when rendering views' do
+  #     render_views
 
-      it { is_expected.to render_template :show }
-    end
-  end
+  #     it { is_expected.to render_template :show }
+  #   end
+  # end
 
-  describe '#edit' do
+  # describe '#edit' do
 
-    context 'when post exists' do
-      
-      subject { get :edit, id: saved_post }
+  #   context 'when post exists' do
+     
+  #     subject { get :edit, id: saved_post }
 
-      context 'when current user matches to post' do
+  #     context 'when current user matches to post' do
 
-        before { sign_in saved_post.user }
+  #       before { sign_in saved_post.user }
 
-        it 'assigns @post' do
-          get :edit, id: saved_post
+  #       it 'assigns @post' do
+  #         get :edit, id: saved_post
 
-          expect(assigns(:post)).to eq(saved_post)
-        end
+  #         expect(assigns(:post)).to eq(saved_post)
+  #       end
 
-        context 'when rendering views' do
-          render_views
+  #       context 'when rendering views' do
+  #         render_views
 
-          it { is_expected.to render_template :edit }
-        end
-      end
+  #         it { is_expected.to render_template :edit }
+  #       end
+  #     end
 
-      context 'when current user doesn\'t match to post' do
+  #     context 'when current user doesn\'t match to post' do
 
-        before { sign_in user }
+  #       before { sign_in user }
 
-        it 'renders #index' do
-          expect(subject).to render_template :index
-        end
-      end
-    end
+  #       it 'renders #index' do
+  #         expect(subject).to render_template :index
+  #       end
+  #     end
+  #   end
 
-    context 'when post does\'t exist' do
-      before { sign_in user }
-      
-      subject { get :edit, id: Faker::Number.number(10) }
+  #   context 'when post does\'t exist' do
+  #     before { sign_in user }
+     
+  #     subject { get :edit, id: Faker::Number.number(10) }
 
-      it { is_expected.to render_template :index }
-    end
-  end
+  #     it { is_expected.to render_template :index }
+  #   end
+  # end
 
-  describe '#update' do
+  # describe '#update' do
 
-    before { sign_in user }
+  #   before { sign_in user }
 
-    context 'with params' do
+  #   context 'with params' do
 
-      it do
-        is_expected.to permit(:title, 
-          :content, 
-          :user_id).for(:update, params: { :id => saved_post, post: attributes_for(:post) })
-      end
-    end
+  #     it do
+  #       is_expected.to permit(:title,
+  #         :content,
+  #         :user_id).for(:update, params: { :id => saved_post, post: attributes_for(:post) })
+  #     end
+  #   end
 
-    context "with valid attributes" do
+  #   context "with valid attributes" do
 
-      before do
-        @new_title = Faker::Lorem.sentence(1)
-        patch :update, :id => saved_post, post: attributes_for(:post, title: @new_title)
-      end
+  #     before do
+  #       @new_title = Faker::Lorem.sentence(1)
+  #       patch :update, :id => saved_post, post: attributes_for(:post, title: @new_title)
+  #     end
 
-      it "updates a post" do
-        expect(Post.find(saved_post.id).title).to eq(@new_title)
-      end
+  #     it "updates a post" do
+  #       expect(Post.find(saved_post.id).title).to eq(@new_title)
+  #     end
 
-      it "redirects to #show" do
-        expect(response).to redirect_to blogs_path saved_post     
-      end
-    end
+  #     it "redirects to #show" do
+  #       expect(response).to redirect_to blogs_path saved_post    
+  #     end
+  #   end
 
-    context "with invalid attributes" do
-      before { patch :update, :id => saved_post, post: attributes_for(:post, title: nil) }
+  #   context "with invalid attributes" do
+  #     before { patch :update, :id => saved_post, post: attributes_for(:post, title: nil) }
 
-      it 'doesn\'t update a post' do
-        expect(Post.find(saved_post.id).title).to eq(saved_post.title)
-      end
+  #     it 'doesn\'t update a post' do
+  #       expect(Post.find(saved_post.id).title).to eq(saved_post.title)
+  #     end
 
-      it { is_expected.to render_template :edit }
-    end
-  end
+  #     it { is_expected.to render_template :edit }
+  #   end
+  # end
 
-  describe '#destroy' do
+  # describe '#destroy' do
 
-    context 'when post exists' do
+  #   context 'when post exists' do
 
-      subject { delete :destroy, :id => saved_post }
+  #     subject { delete :destroy, :id => saved_post }
 
-      context 'when current user matches to post' do
+  #     context 'when current user matches to post' do
 
-        before { sign_in saved_post.user }
+  #       before { sign_in saved_post.user }
 
-        it 'destroys' do
-          expect { subject }.to change(Post, :count).by(-1)
-        end
+  #       it 'destroys' do
+  #         expect { subject }.to change(Post, :count).by(-1)
+  #       end
 
-        it { is_expected.to redirect_to blogs_root_path }
-      end
+  #       it { is_expected.to redirect_to blogs_root_path }
+  #     end
 
-      context 'when current user doesn\'t match to post' do
+  #     context 'when current user doesn\'t match to post' do
 
-        before { sign_in user }
+  #       before  do
+  #         sign_out saved_post.user
+  #         sign_in user
+  #       end
 
-        it 'doesn\'t destroy' do
+  #       it 'doesn\'t destroy' do
 
-          expect(Post.find_by(id: saved_post.id)).to_not be_nil
-        end
+  #         expect { subject }.to_not change(Post, :count)
+  #       end
 
-        it { is_expected.to redirect_to blogs_root_path }
-      end
-    end
+  #       it { is_expected.to redirect_to blogs_root_path }
+  #     end
+  #   end
 
-    context 'when post doesn\'t exist' do
-      
-      before { sign_in user }
+  #   context 'when post doesn\'t exist' do
+     
+  #     before { sign_in user }
 
-      subject { delete :destroy, :id => Faker::Number.number(3) }
+  #     subject { delete :destroy, :id => saved_post }
 
-      it { is_expected.to redirect_to blogs_root_path }
-    end
-  end
+  #     it { is_expected.to redirect_to blogs_root_path }
+  #   end
+  # end
 end
